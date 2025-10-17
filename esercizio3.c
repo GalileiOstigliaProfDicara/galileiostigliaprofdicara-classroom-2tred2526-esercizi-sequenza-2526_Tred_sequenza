1f6faa921331fd945381cb0b84710422ba01817e
Dicara Francesco <dicaraf@gmail.com>
2025-10-17 09:59:52 +0000

    primi esempi

---
+++ esercizio3.c
@@ -0,0 +1,15 @@
+//Chieste all'utente le dimensioni di un rettangolo, calcola e mostra area e 
+//perimetro.
+
+#include <stdio.h>
+
+int main() {
+    int base, altezza, area, perimetro;
+    printf("Inserisci base e altezza del rettangolo: ");
+    scanf("%d", &base);
+    scanf("%d", &altezza);
+
+    area = base * altezza;
+    perimetro = (base + altezza) * 2;
+    printf("L'area del rettangolo è %d e il perimetro è %d", area, perimetro);
+}
