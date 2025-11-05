1662206f59b9f9e0351df89d984469f71a4ef758
Dicara Francesco <dicaraf@gmail.com>
2025-11-05 14:35:18 +0000

    esercizio caramelle

--- esercizio3.c
+++ esercizio3.c
@@ -8,7 +8,6 @@ int main() {
     printf("Inserisci base e altezza del rettangolo: ");
     scanf("%d", &base);
     scanf("%d", &altezza);
-
     area = base * altezza;
     perimetro = (base + altezza) * 2;
     printf("L'area del rettangolo è %d e il perimetro è %d", area, perimetro);
